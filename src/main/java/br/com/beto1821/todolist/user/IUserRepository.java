7e2dae9792c6efb1d4254662c2aa605438b74ac4
Adalberto R. Ribeiro <beto1821@uol.com.br>
2023-10-13 11:28:02 -0300

    fix: Interface

--- src/main/java/br/com/beto1821/todolist/user/IUserRepository.java
+++ src/main/java/br/com/beto1821/todolist/user/IUserRepository.java
@@ -3,9 +3,8 @@ package br.com.beto1821.todolist.user;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 
 public interface IUserRepository extends JpaRepository<UserModel, UUID> {
-    List<UserModel> findByUsername(String username);
+    UserModel findByUsername(String username);
 }
